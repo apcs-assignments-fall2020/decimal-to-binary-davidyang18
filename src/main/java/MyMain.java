4aa7b3e1bde659951258dc3eef80b2c13f1cf9dd
davidyang18 <2817822828@qq.com>
2020-09-17 04:40:58 +0000

    Binary To Decimal

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -3,12 +3,16 @@ import java.util.Scanner;
 public class MyMain {
     
     public static int binaryToDecimal(String binary) { 
-        return -1;
-        // REPLACE WITH YOUR CODE HERE
+        int decimal = Integer.parseInt(binary,2);
+        return decimal;
     }
     
     
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        Scanner scan = new Scanner (System.in);
+        String bi_num = scan.next();
+        System.out.println("Please enter a binary number.");
+        System.out.println("Your binary input is " + binaryToDecimal(bi_num) + " in decimal.");
+        scan.close();
     }
 }
